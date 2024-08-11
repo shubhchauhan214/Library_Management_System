ab248b9cc81bf2fe0ffd3cbd03239d611d232d9a
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-08-11 21:00:26 +0530

    Set up Database

---
+++ models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
